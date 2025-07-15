69c312a34a9e842c128a1d2c3f2d792b7b9e0493
Bhavin Parghi <bhavin.parghi007@gmail.com>
2025-07-16 00:54:57 +0530

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.snap_check"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.1.12297006"
 
 
     compileOptions {
